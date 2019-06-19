e23eb7a9d0db83d2f0785c6d2844d7e2056c6082
fahim <fahimvalanchery@gmail.com>
2019-06-19 15:21:48 +0530

    navigation basics

--- App.js
+++ App.js
@@ -1,49 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import HomeSreen from "./scr/HomeSreen";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {
+  createStackNavigator,
+  createAppContainer,
+  createDrawerNavigator
+} from "react-navigation";
+import SettingsScreen from "./scr/SettingsScreen";
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
+/*
+const AppStackNavigator = createStackNavigator(
+  {
+    Home: HomeSreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#455a64"
+      }
+    }
+  }
+);*/
+const AppStackNavigator = createDrawerNavigator(
+  {
+    Setting: SettingsScreen,
+    Home: HomeSreen
+  },
+  {
+    unmountInactiveRoutes:true
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#455a64"
+      }
+    }
+  }
+);
+
+const AppContainer = createAppContainer(AppStackNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
---
+++ scr/SettingsScreen.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+export default class SettingsScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>SettingsScreen</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
